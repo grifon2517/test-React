a2e2644d5ba3e8b81ca0e205b425d2d6de5a2fac
Gregory Chebatyuk <gchebatyuk@gmail.com>
2026-01-15 11:50:04 +0300

    react-hook-form+yup

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,30 +1,44 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+
 import styles from './app.module.css';
 
+const fieldsScheme = yup.object().shape({
+	login: yup
+		.string()
+		.matches(/^[\w_]*$/, 'Должны использоваться буквы, цифры и нижнее подчеркивание')
+		.max(20, 'Должно быть более 20 символов')
+		.min(3, 'Должно быть не менее 3 символов'),
+});
+
 export const App = () => {
-	const [selectedProducts, setSelectedProducts] = useState('tv');
-	const [selectedColors, setSelectedColors] = useState(['red', 'green']);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		defaultValues: {
+			login: '',
+		},
+		resolver: yupResolver(fieldsScheme),
+	});
 
-	const onSelectedProductChange = ({ target }) => setSelectedProducts(target.value);
-	const onSelectedColors = ({ target }) => {
-		const newSelectedColors = [...target.selectedOptions].map(
-			(selectedTarget) => selectedTarget.value,
-		);
-
-		setSelectedColors(newSelectedColors);
+	const loginError = errors.login?.message;
+	const onSubmit = (formData) => {
+		console.log(formData);
 	};
+
 	return (
 		<div className={styles.app}>
-			<select value={selectedProducts} onChange={onSelectedProductChange}>
-				<option value="TV">Телевизор</option>
-				<option value="pad">Планшет</option>
-				<option value="phone">Телефон</option>
-			</select>
-			<select multiple={true} value={selectedColors} onChange={onSelectedColors}>
-				<option value="red">Красный</option>
-				<option value="green">Зеленый</option>
-				<option value="white">Белый</option>
-			</select>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				{loginError && <div className={styles.errorLabel}>{loginError}</div>}
+				<input name="login" type="text" {...register('login')} />
+				<button type="submit" disabled={!!loginError}>
+					Отправить
+				</button>
+			</form>
 		</div>
 	);
 };
